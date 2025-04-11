45da8f0f540576720487d372877bb298c7177c30
lucasvaladaoescola <LucasValadaoEscola@gmail.com>
2025-04-11 14:53:55 -0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+            <section>
+                <h1>Processamento de Linguagem Natural (NLP)</h1>
+                <br />
+                <br />
+                <br />
+                <br />
+                <h2>🧠 O que é Processamento de Linguagem Natural (NLP)?
+                <br />
+                <br />
+                O Processamento de Linguagem Natural (NLP) é uma área da Inteligência Artificial que permite que computadores entendam, interpretem e gerem a linguagem humana. Ele é responsável por tornar possível a comunicação entre pessoas e máquinas usando linguagem natural — como falamos ou escrevemos no dia a dia.</h2>
+                <br />
+                <br />
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Kaz-osMcdxk?si=AuUcFXRjCMrnz72Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <h3>💡 Por que o NLP é importante?
+                    <br />
+                    <br />
+                    A linguagem humana é complexa, cheia de ambiguidades, emoções, gírias e contextos. O NLP ajuda a transformar essa linguagem em algo que as máquinas conseguem compreender e responder de maneira inteligente. Isso melhora a forma como interagimos com a tecnologia e automatiza tarefas que antes exigiam interpretação humana.</h3>
+               
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/X1IpNUs1PV8?si=LnxqlBlR-G07-dFs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <br />
+                    <br />
+                    <h4> 📱 Onde o NLP é usado?
+                    <br />
+                    <br />
+                    1- Assistentes virtuais (Siri, Alexa, Google Assistente)
+                     <br />
+                     <br />
+                     2- Tradutores automáticos (como o Google Tradutor)
+                     <br />
+                    <br />
+                    3- Chatbots e atendimento automatizado
+                    <br />
+                    <br />
+                    4- Análise de sentimentos em redes sociais
+                    <br />
+                    <br />
+                    5- Corretores ortográficos e de digitação
+                    <br />
+                    <br />
+                    6- Filtros de spam em e-mails
+                    <br />
+                    <br />
+                    7- Sistemas de busca inteligente
+                    <br />
+                    <br />
+                    <br />
+                    </h4>
+                     <h5>Desenvolvido por Lucas Valadão De A. Marsaioli </h5>
+            </section>
+        </>
+    );
+};
+export default Content;
