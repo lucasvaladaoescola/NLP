a4c7484359e401d2969d72131e5b879f21d8d1b6
lucasvaladaoescola <LucasValadaoEscola@gmail.com>
2025-04-15 04:54:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,10 @@ export default defineConfig({
   plugins: [react()],
   base: "/Intelig-ncia-artificial"
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './Intelig-ncia-artificial', // 
+})
